d0f59d4c020bac8d65968aa7c8cb8991a08e3001
Farhana Kabir <farhanakabir99@gmail.com>
2022-09-01 11:07:38 -0400

    changed weeklyplanner

--- frontend/src/Components/WeeklyPlanner/WeeklyPlanner.js
+++ frontend/src/Components/WeeklyPlanner/WeeklyPlanner.js
@@ -1,54 +1,64 @@
- import React from "react";
-import {Link} from 'react-router-dom';
- import yellowdot from '../../images/yellowdot.png'
-import bluedot from '../../images/bluedot.png'
-import greendot from '../../images/greendot.png'
-import purpledot from '../../images/purpledot.png'
-import orangedot from '../../images/orangedot.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import yellowdot from "../../images/yellowdot.png";
+import bluedot from "../../images/bluedot.png";
+import greendot from "../../images/greendot.png";
+import purpledot from "../../images/purpledot.png";
+import orangedot from "../../images/orangedot.png";
 
- function weeklyplanner(){
-    return(
-        <div> 
-            <h1> Weekly Planner</h1>
-            <div className="weeklycontaner">
-            <ul>
-              <Link to="/weeklyplanner" className="link">
-                    <li id='li_weekly'>
-                        <img src={yellowdot} alt='large yellow ellipse'/>
-                    Monday 
-                    </li>
-                </Link>
-                <li id='li_saved'>
-                <img src={orangedot} alt='large orange dot'/>
-                    Tuesday
-                </li>
-                <li id='li_pantry'>
-                <img src={greendot} alt='large green dot'/>
-                    Wednesday
-                </li>
-                <li id='li_shopping'>
-                <img src={bluedot} alt='large blue dot'/>
-                    Thursday
-                </li>
-                <li id='li_create'>
-                <img src={purpledot} alt='large purple dot'/>
-                    Friday
-                </li>
-                <li id='li_create'>
-                <img src={purpledot} alt='large purple dot'/>
-                    Saturday
-                </li>
-                <li id='li_create'>
-                <img src={purpledot} alt='large purple dot'/>
-                    Sunday
-                </li>
-            </ul>
+function weeklyplanner() {
+  return (
+    <div>
+      <h1> Weekly Planner</h1>
+      <div className="weeklycontaner">
+        <ul>
+          <li id="monday">
+            <img src={yellowdot} alt="large yellow dot" />
+            <Link to="/day" onClick={(e) => setDay(e.target.innerText)}>
+              Monday
+            </Link>
+          </li>
 
-            </div>
-
-        </div>
-
-    )
+          <li id="tuesday">
+            <img src={orangedot} alt="large orange dot" />
+            <Link to="/day" onClick={(e) => setDay(e.target.innerText)}>
+              Tuesday
+            </Link>
+          </li>
+          <li id="wednesday">
+            <img src={greendot} alt="large green dot" />
+            <Link to="/day" onClick={(e) => setDay(e.target.innerText)}>
+              Wednesday
+            </Link>
+          </li>
+          <li id="thursday">
+            <img src={bluedot} alt="large blue dot" />
+            <Link to="/day" onClick={(e) => setDay(e.target.innerText)}>
+              Thursday
+            </Link>
+          </li>
+          <li id="friday">
+            <img src={purpledot} alt="large purple dot" />
+            <Link to="/day" onClick={(e) => setDay(e.target.innerText)}>
+              Friday
+            </Link>
+          </li>
+          <li id="saturday">
+            <img src={purpledot} alt="large purple dot" />
+            <Link to="/day" onClick={(e) => setDay(e.target.innerText)}>
+              Saturday
+            </Link>
+          </li>
+          <li id="sunday">
+            <img src={purpledot} alt="large purple dot" />
+            <Link to="/day" onClick={(e) => setDay(e.target.innerText)}>
+              Sunday
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
- export default  weeklyplanner;
+export default weeklyplanner;
