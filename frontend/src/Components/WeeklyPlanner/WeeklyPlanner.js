1da89507f6961c3f3322672c1004109a31fe5ae7
Farhana Kabir <farhanakabir99@gmail.com>
2022-09-01 00:26:56 -0400

    weekly planner page created

--- frontend/src/Components/WeeklyPlanner/WeeklyPlanner.js
+++ frontend/src/Components/WeeklyPlanner/WeeklyPlanner.js
@@ -1,42 +1,54 @@
-import React from 'react';
-import './landingpage.css';
-import yellowdot from '../../images/yellowdot.png'
+ import React from "react";
+import {Link} from 'react-router-dom';
+ import yellowdot from '../../images/yellowdot.png'
 import bluedot from '../../images/bluedot.png'
 import greendot from '../../images/greendot.png'
 import purpledot from '../../images/purpledot.png'
 import orangedot from '../../images/orangedot.png'
 
-import { Link } from 'react-router-dom';
-
-
-export default function LandingPage(){
-
-    return (
-        <div>
+ function weeklyplanner(){
+    return(
+        <div> 
+            <h1> Weekly Planner</h1>
+            <div className="weeklycontaner">
             <ul>
               <Link to="/weeklyplanner" className="link">
                     <li id='li_weekly'>
                         <img src={yellowdot} alt='large yellow ellipse'/>
-                    Weekly Planner 
+                    Monday 
                     </li>
                 </Link>
                 <li id='li_saved'>
                 <img src={orangedot} alt='large orange dot'/>
-                    Saved Recipes
+                    Tuesday
                 </li>
                 <li id='li_pantry'>
                 <img src={greendot} alt='large green dot'/>
-                    Pantry
+                    Wednesday
                 </li>
                 <li id='li_shopping'>
                 <img src={bluedot} alt='large blue dot'/>
-                    Shopping List
+                    Thursday
                 </li>
                 <li id='li_create'>
                 <img src={purpledot} alt='large purple dot'/>
-                    Create Recipe
+                    Friday
+                </li>
+                <li id='li_create'>
+                <img src={purpledot} alt='large purple dot'/>
+                    Saturday
+                </li>
+                <li id='li_create'>
+                <img src={purpledot} alt='large purple dot'/>
+                    Sunday
                 </li>
             </ul>
+
+            </div>
+
         </div>
+
     )
 }
+
+ export default  weeklyplanner;
