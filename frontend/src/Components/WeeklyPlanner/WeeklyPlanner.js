a9ba445fd683a2dfceda51fea582351b2100ef32
Farhana Kabir <farhanakabir99@gmail.com>
2022-09-01 11:19:15 -0400

    added link and img to meal planner

--- frontend/src/Components/WeeklyPlanner/WeeklyPlanner.js
+++ frontend/src/Components/WeeklyPlanner/WeeklyPlanner.js
@@ -15,44 +15,54 @@ export default function WeeklyPlanner() {
     <div>
       <Navigator />
       <div>
+      <h1> Weekly Planner</h1>
+      <div className="weeklycontaner">
         <ul>
           <li id="monday">
-            <Link to="/day" onClick={(e) => setDay(e.target.innerText)} >
+            <img src={yellowdot} alt="large yellow dot" />
+            <Link to="/day" onClick={(e) => setDay(e.target.innerText)}>
               Monday
             </Link>
           </li>
           <li id="tuesday">
+            <img src={orangedot} alt="large orange dot" />
             <Link to="/day" onClick={(e) => setDay(e.target.innerText)}>
               Tuesday
             </Link>
           </li>
           <li id="wednesday">
+            <img src={greendot} alt="large green dot" />
             <Link to="/day" onClick={(e) => setDay(e.target.innerText)}>
               Wednesday
             </Link>
           </li>
           <li id="thursday">
+            <img src={bluedot} alt="large blue dot" />
             <Link to="/day" onClick={(e) => setDay(e.target.innerText)}>
               Thursday
             </Link>
           </li>
           <li id="friday">
+            <img src={purpledot} alt="large purple dot" />
             <Link to="/day" onClick={(e) => setDay(e.target.innerText)}>
               Friday
             </Link>
           </li>
           <li id="saturday">
+            <img src={purpledot} alt="large purple dot" />
             <Link to="/day" onClick={(e) => setDay(e.target.innerText)}>
               Saturday
             </Link>
           </li>
           <li id="sunday">
+            <img src={purpledot} alt="large purple dot" />
             <Link to="/day" onClick={(e) => setDay(e.target.innerText)}>
               Sunday
             </Link>
           </li>
         </ul>
       </div>  
+    </div>
     </div>
   );
 }
