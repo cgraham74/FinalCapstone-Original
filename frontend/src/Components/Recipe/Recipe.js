df5991ddcbaa9c6fd4c0c263e0c59857d71c2acd
ChristinaGraham <cgraham74@gmail.com>
2022-09-02 13:52:56 -0500

    Working on Recipe.js adding functionality

--- frontend/src/Components/Recipe/Recipe.js
+++ frontend/src/Components/Recipe/Recipe.js
@@ -2,70 +2,112 @@ import React from "react";
 import SavedRecipes from "./SavedRecipes";
 import Navigator from "../navigation/Navigator";
 import { useState, useCallback, useEffect } from "react";
+import { addToken } from "../../Redux/actionCreators";
+import { baseUrl } from "../../Shared/baseUrl";
 
-const BASE_URL ="#";
+const Recipe = () => {
+  const [recipeCollection, setRecipeCollection] = useState("");
+  const [recipeCard, setRecipeCard] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEdited, setIsEdited] = useState(false);
 
- const Recipe = () => {
-    const [recipeCollection, setRecipeCollection] = useState("");
-    const[recipeCard, setRecipeCard] = useState({});
-    const [isLoading, setIsLoading] = useState(false);
-    const [isEdited, setIsEdited] = useState(false);
+  /**
+   * A function provided to delete unwanted recipes
+   *
+   * @param {*} event
+   * @param {*} id
+   */
+  function deleteHandler(event, id) {
+    fetch(baseUrl + "/" + id, {
+      method: "DELETE",
+      cache: "no-cache",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${addToken}`,
+      },
+    })
+      .then((response) => {
+        setIsLoading(!isLoading);
+        return response.text();
+      })
+      .then((data) => {
+        console.log(data);
+        alert("Recipe deleted!");
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("Could not delete recipe!");
+      });
+  }
 
-    /**
-     * A function provided to delete unwanted recipes
-     * 
-     * @param {*} event 
-     * @param {*} id 
-     */
-    function deleteHandler(event, id) {
-        fetch(BASE_URL + "/" + id, {
-          method: "DELETE",
-        })
-          .then((response) => {
-            setIsLoading(!isLoading);
-            return response.text();
-          })
-          .then((data) => {
-            console.log(data);
-            alert("Recipe deleted!");
-          })
-          .catch((err) => {
-            console.error(err);
-            alert("Could not delete Recipe!");
-          });
-      }
+  /**
+   * Handler method to update a specific recipe
+   * @param {*} event
+   * @param {*} id
+   */
+  function editHandler(event, id) {
+    fetch(baseUrl + "/" + id, {
+      method: "PUT",
+      cache: "no-cache",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${addToken}`,
+      },
+      body: JSON.stringify(recipeCard),
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert("Saved Recipe!");
+          setIsEdited(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Could not save recipe");
+      });
+  }
 
-      function editHandler(event, id){
+  /**
+   * Fetch to get a single recipe by its id
+   */
+  const fetchRecipeCard = useCallback((id) => {
+    fetch(baseUrl + "/" + id)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => setRecipeCard(data));
+    console.log(recipeCard);
+  }, []);
 
-      }
+  /**
+   *
+   */
+  const fetchCollection = useCallback(() => {
+    fetch(baseUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => setRecipeCollection(data));
+  }, []);
 
-      /**
-       * 
-       */
-      const fetchCollection = useCallback(() => {
-        fetch(BASE_URL)
-          .then((response) => {
-            return response.json();
-          })
-          .then((data) => setRecipeCollection(data));
-      
-      }, []);
+  useEffect(() => {
+    fetchRecipeCard();
+  }, []);
 
-      
   useEffect(() => {
     fetchCollection();
   }, [isLoading, isEdited, fetchCollection]);
   return (
     <>
       <Navigator />
-      <SavedRecipes 
-    recipeCard={recipeCard}
-    recipeCollection={recipeCollection}
-    deleteHandler={deleteHandler}
-    editHandler={editHandler}
-    />
+      <SavedRecipes
+        recipeCard={recipeCard}
+        recipeCollection={recipeCollection}
+        deleteHandler={deleteHandler}
+        editHandler={editHandler}
+      />
     </>
   );
-}
+};
 
 export default Recipe;
