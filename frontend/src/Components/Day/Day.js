1b4590e63c4e594f18619e0bb5484ede44e73956
Farhana Kabir <farhanakabir99@gmail.com>
2022-09-07 12:57:03 -0400

    added buttons and style in nav-bar and added brekfast item in meal plan

--- frontend/src/Components/Day/Day.js
+++ frontend/src/Components/Day/Day.js
@@ -2,6 +2,8 @@ import { React, useState, useEffect } from "react";
 import Navigator from "../navigation/Navigator";
 import { useLocation } from "react-router-dom";
 import { baseUrl } from "../../Shared/baseUrl";
+import { Button,Col } from "reactstrap";
+import "./day.css";
 
 export default function Day(props) {
   const location = useLocation();
@@ -9,7 +11,25 @@ export default function Day(props) {
 
   const [newRecipes, setnewRecipes] = useState([{}]);
   const [posts, setPosts] = useState([]);
-   
+  let [num, setNum] = useState(0);
+
+  const [isAdded, setIsAdded] = useState(false);
+  const [list, setList] = useState([]);
+  //const dispatch = useContext(CartDispatchContext);
+  //const { image, name, price, id, stock } = data;
+
+  const handleAddToCart = (e) => {
+    // const product = { ...data, quantity: 1 };
+    // addToCart(dispatch, product);
+    setIsAdded(true);
+    setList([...list, e.target.value]);
+    list.map((item) => {
+      alert(item+" "+num);
+    });
+    setTimeout(() => {
+      setIsAdded(false);
+    }, 3500);
+  };
   //  useEffect(() => {
   //     let mounted= true;
 
@@ -31,31 +51,99 @@ export default function Day(props) {
   //         })
   //         .then(response=>response.json());
   //  }
-   useEffect(() => {
-    fetch(baseUrl+'/Test/RecipeListTest')
-       .then((response) => response.json())
-       .then((data) => {
-          console.log(data);
-          setPosts(data);
-          
-       })
-       .catch((err) => {
-          console.log(err.message);
-       });
- }, []);
+  useEffect(() => {
+    fetch(baseUrl + "/Test/TestBreakfast")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
   return (
     <>
       <Navigator />
+
+      <h1 id="meal-day">
+        Available {location.state.meal} item for {location.state.day} is:
+      </h1>
       
-      <h1>selected day is {location.state.day}</h1>
-      <h1>selected meal is {location.state.meal}</h1>
       <div>
-        {posts.map((post)=>{
-            return(
-            <h1>available meal {post.title}</h1>)
+        
+        <br/>
+        {posts.map((post) => {
+          return (
+            <>
+              <h1>{post}</h1>
+              <div className="product-action">
+                {IncDecCounter(num, setNum)}
+                <br />
+                <Button
+                  //className={!isAdded ? "" : "added"}
+                  color= "success"
+                  type="button"
+                  value={post}
+                  onClick={handleAddToCart}
+                  disabled={num===0}
+                >
+                  {!isAdded ? "ADD TO CART" : "✔ ADDED"}
+                </Button>
+              </div>
+            </>
+          );
         })}
       </div>
-      
+    </>
+  );
+}
+
+function IncDecCounter(num, setNum) {
+  let incNum = () => {
+    if (num < 10) {
+      setNum(Number(num) + 1);
+    }
+  };
+  let decNum = () => {
+    if (num > 0) {
+      setNum(num - 1);
+    }
+  };
+  let handleChange = (e) => {
+    setNum(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="col-2 col-md-1">
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <button
+              class="btn btn-outline-primary"
+              type="button"
+              onClick={decNum}
+            >
+              -
+            </button>
+          </div>
+          <input
+            type="text"
+            class="form-control"
+            value={num}
+            onChange={handleChange}
+          />
+          <div class="input-group-prepend">
+            <button
+              class="btn btn-outline-primary"
+              type="button"
+              onClick={incNum}
+            >
+              +
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- frontend/src/Components/navigation/Navigator.js
+++ frontend/src/Components/navigation/Navigator.js
@@ -1,23 +1,34 @@
 import React from "react";
-import { ButtonGroup, Button } from 'reactstrap'
-import './navigation.css';
+import { ButtonGroup, Button } from "reactstrap";
+import "./navigation.css";
 import { Link } from "react-router-dom";
 
-function Navigator () {
-
-    return (
-        <nav id="navigation_container">
-            <h1>Meal Planner</h1>
-            <div id='nav_container'>
-                <ButtonGroup id="nav_btngrp">
-                    <Button id="nav_planner"><Link to='/weeklyplanner'>Planner</Link></Button>
-                    <Button id="nav_recipes"><Link to='/savedrecipes'>Recipes</Link></Button>
-                    <Button id="nav_pantry"><Link to='/pantry'>Pantry</Link></Button>
-                    <Button id="nav_shopping"><Link to='/shoppinglist'>Shopping</Link></Button>
-                </ButtonGroup>
-            </div>
-        </nav>
-    )
+function Navigator() {
+  return (
+    <nav id="navigation_container">
+      <h1>Meal Planner</h1>
+      <div id="nav_container">
+        <ButtonGroup id="nav_btngrp">
+          <Button id="nav_home">
+            <Link to="/">Home</Link>
+          </Button>
+          <Button id="nav_planner">
+            <Link to="/weeklyplanner">Planner</Link>
+          </Button>
+          <Button id="nav_recipes">
+            <Link to="/recipes">Recipes</Link>
+          </Button>
+          <Button id="nav_pantry">
+            <Link to="/pantry">Pantry</Link>
+          </Button>
+          <Button id="nav_shopping">
+            <Link to="/shoppinglist">Shopping</Link>
+          </Button>
+          <Button id="nav_createrecipe"><Link to='/createrecipe'>CreateRecipe</Link></Button>
+        </ButtonGroup>
+      </div>
+    </nav>
+  );
 }
 
 export default Navigator;
