3d6a56397e06e74dd5c8938ca614f31fefcd401b
ChristinaGraham <cgraham74@gmail.com>
2022-09-19 10:42:50 -0500

    added CSS to saveall

--- frontend/src/Components/Pantry/AllRecipes.js
+++ frontend/src/Components/Pantry/AllRecipes.js
@@ -1,19 +1,22 @@
 import React from "react";
 import Navigator from "../navigation/Navigator";
 import RecipeCard from "../Recipe/RecipeCard";
-import "./pantry.css";
+import "./allrecipe.css";
 
 export default function AllRecipes(props) {
   return (
     <>
       <Navigator />
       <h3>All Recipes</h3>
+<div id="render-all-recipes">
+
 
       <RecipeCard
         allrecipes={props.allrecipes}
-        //  recipeCard={item}
+      
         token={props.token}
       />
+      </div>
     </>
   );
 }
